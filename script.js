3f49628752e63957390b615479af68b6e562510c
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-11-17 09:53:28 +0000

    fix getting body for mcqs

--- script.js
+++ script.js
@@ -36,9 +36,8 @@ function convertHtmlToJson() {
   var h1Elements = doc.getElementsByTagName("h1");
   for (var i = 0; i < h1Elements.length; i++) {
     var h1 = h1Elements[i];
-    console.log(h1.textContent);
+    // console.log(h1.textContent);
     var body = "";
-
 
     var nextElem = h1.nextElementSibling;
     var h2Array = [];
@@ -47,7 +46,8 @@ function convertHtmlToJson() {
 
     while (nextElem && nextElem.tagName !== "H1") {
       if (h2Array.length === 0 && !h1.textContent.includes("?")) {
-         body += nextElem.outerHTML;
+        // but remove the h2 tag after it
+        body += nextElem.outerHTML.replace(/<h2.*h2>/g, "");
       }
       if (nextElem.tagName === "H2") {
         var h2Obj = {
@@ -65,38 +65,62 @@ function convertHtmlToJson() {
       }
       if (h1.textContent.includes("Q:")) {
         if (nextElem.tagName === "OL" || nextElem.tagName === "UL") {
-            // loop through li elements and create options array
-            const liElements = nextElem.getElementsByTagName("li");
-            for (var j = 0; j < liElements.length; j++) {
-                const li = liElements[j];
-                let option = {
-                    text: li.textContent,
-                    _shouldBeSelected: li.innerHTML.includes("<strong>") ? true : false,
-                    _isPartlyCorrect: false,
-                };
-                options.push(option);
-            }
-    
-            // After processing the list, traverse the siblings to collect feedback
-            let feedbackText = "";
-            let currentElem = nextElem.nextElementSibling;
-    
-            while (currentElem && currentElem.tagName !== "H1") {
-                feedbackText += currentElem.outerHTML;
-                currentElem = currentElem.nextElementSibling;
-            } 
-            console.log(feedbackText);
-    
-            const feedbackSplit = feedbackText.split("</strong>");
-            feedback.correct = feedbackSplit[1];
-    
-
-            feedback.correct = '<p><strong>' + "That's right!" + '</p></strong>' + feedbackSplit[1]
-            feedback._incorrect = {
-                notFinal: '<p><strong>' + "Are you sure about that?" + '</p></strong>' + feedbackSplit[1],
-                final: '<p><strong>' + "Are you sure about that?" + '</p></strong>' + feedbackSplit[1]
+          // loop through li elements and create options array
+          const liElements = nextElem.getElementsByTagName("li");
+          for (var j = 0; j < liElements.length; j++) {
+            const li = liElements[j];
+            let option = {
+              text: li.textContent,
+              _shouldBeSelected: li.innerHTML.includes("<strong>")
+                ? true
+                : false,
+              _isPartlyCorrect: false,
             };
+            options.push(option);
+          }
+
+          // After processing the list, traverse the siblings to collect feedback
+          let feedbackText = "";
+          let currentElem = nextElem.nextElementSibling;
+
+          while (currentElem && currentElem.tagName !== "H1") {
+            feedbackText += currentElem.outerHTML;
+            currentElem = currentElem.nextElementSibling;
+          }
+          console.log(feedbackText);
+
+          const feedbackSplit = feedbackText.split("</strong>");
+          feedback.correct = feedbackSplit[1];
+
+          feedback.correct =
+            "<p><strong>" +
+            "That's right!" +
+            "</p></strong>" +
+            feedbackSplit[1];
+          feedback._incorrect = {
+            notFinal:
+              "<p><strong>" +
+              "Are you sure about that?" +
+              "</p></strong>" +
+              feedbackSplit[1],
+            final:
+              "<p><strong>" +
+              "Are you sure about that?" +
+              "</p></strong>" +
+              feedbackSplit[1],
+          };
         }
+        // if elemenet after h1 is a p tag, add add it body but stop when you reach the next an element that says this <p><strong>
+
+        // Check if the next element is a P tag and does not include a strong tag
+        // if (nextElem.tagName === "P") {
+        //   // If the P tag contains a strong tag, we stop adding to body
+        //   if (nextElem.innerHTML.includes("<strong>")) {
+        //     break; // Exit the loop as we've encountered a P with a strong tag
+        //   } else {
+        //     body += nextElem.innerHTML; // Add the P tag to body
+        //   }
+        // }
       }
       nextElem = nextElem.nextElementSibling;
     }
@@ -106,13 +130,13 @@ function convertHtmlToJson() {
     const moduleId = document.getElementById("moduleID").value;
     const articleID = document.getElementById("articleID").value;
 
-    var blockID = moduleId + "b-" + (i + 1);
-    var componentID = moduleId + "c-" + (i + 1);
-    var graphicID = moduleId + "g-" + (i + 1);
+    var blockID = moduleId + "-b-" + (i + 1);
+    var componentID = moduleId + "-c-" + (i + 1);
+    var graphicID = moduleId + "-g-" + (i + 1);
 
     var blockObj = {
       title: h1.textContent,
-      displayTitle: h1.textContent.length > 5 ? "" : h1.textContent,
+      displayTitle: h1.textContent,
       body: "",
       _classes: "",
       _trackingId: i + 1,
@@ -136,67 +160,68 @@ function convertHtmlToJson() {
       },
       _pageLevelProgress: {
         _isEnabled: true,
-        _isCompletionIndicatorEnabled: true,
+        _isCompletionIndicatorEnabled: false,
       },
     };
     blockObjects.push(blockObj);
 
-if (h1.textContent.includes("?")) {
-var mcqComponent = {
-  _id: componentID,
-  _parentId: blockID,
-  _type: "component",
-  _component: "mcq",
-  _classes: "",
-  _layout: "left",
-  title: h1.textContent,
-  displayTitle: h1.textContent,
-  body: body,
-  instruction: "Choose one option then select Submit.",
-  ariaQuestion: "Question text specifically for screen readers.",
-  _attempts: 1,
-  _shouldDisplayAttempts: false,
-  _isRandom: false,
-  _hasItemScoring: false,
-  _questionWeight: 1,
-  _selectable: 1,
-  _canShowModelAnswer: true,
-  _canShowFeedback: true,
-  _canShowMarking: true,
-  _recordInteraction: true,
-  _items: options,
-  _feedback: feedback,
-  _comment: "You only need to include _buttons if you want to override the button labels that are set in course.json",
-  _buttons: {
-      _submit: {
-          buttonText: "Submit",
-          ariaLabel: "Select here to submit your answer."
-      },
-      _reset: {
-          buttonText: "Reset",
-          ariaLabel: ""
-      },
-      _showCorrectAnswer: {
-          buttonText: "Correct Answer",
-          ariaLabel: ""
-      },
-      _hideCorrectAnswer: {
-          buttonText: "My Answer",
-          ariaLabel: ""
-      },
-      _showFeedback: {
-          buttonText: "Show feedback",
-          ariaLabel: ""
-      },
-      remainingAttemptsText: "attempts remaining",
-      remainingAttemptText: "final attempt"
-  },
-  _pageLevelProgress: {
-      _isEnabled: false
-  }
-};
-componentObjects.push(mcqComponent);
-} else if (h2Array.length > 0) {
+    if (h1.textContent.includes("Q:")) {
+      var mcqComponent = {
+        _id: componentID,
+        _parentId: blockID,
+        _type: "component",
+        _component: "mcq",
+        _classes: "",
+        _layout: "left",
+        title: h1.textContent,
+        displayTitle: h1.textContent.split("Q:")[1],
+        body: body.split("<ul>")[0],
+        instruction: "Choose one option then select Submit.",
+        ariaQuestion: "Question text specifically for screen readers.",
+        _attempts: 1,
+        _shouldDisplayAttempts: false,
+        _isRandom: false,
+        _hasItemScoring: false,
+        _questionWeight: 1,
+        _selectable: 1,
+        _canShowModelAnswer: true,
+        _canShowFeedback: true,
+        _canShowMarking: true,
+        _recordInteraction: true,
+        _items: options,
+        _feedback: feedback,
+        _comment:
+          "You only need to include _buttons if you want to override the button labels that are set in course.json",
+        _buttons: {
+          _submit: {
+            buttonText: "Submit",
+            ariaLabel: "Select here to submit your answer.",
+          },
+          _reset: {
+            buttonText: "Reset",
+            ariaLabel: "",
+          },
+          _showCorrectAnswer: {
+            buttonText: "Correct Answer",
+            ariaLabel: "",
+          },
+          _hideCorrectAnswer: {
+            buttonText: "My Answer",
+            ariaLabel: "",
+          },
+          _showFeedback: {
+            buttonText: "Show feedback",
+            ariaLabel: "",
+          },
+          remainingAttemptsText: "attempts remaining",
+          remainingAttemptText: "final attempt",
+        },
+        _pageLevelProgress: {
+          _isEnabled: false,
+        },
+      };
+      componentObjects.push(mcqComponent);
+    } else if (h2Array.length > 0) {
       var accordionComponent = {
         _id: componentID,
         _parentId: blockID,
@@ -225,7 +250,7 @@ componentObjects.push(mcqComponent);
           };
         }),
         _pageLevelProgress: {
-          _isEnabled: true,
+          _isEnabled: false,
         },
       };
       componentObjects.push(accordionComponent);
@@ -238,7 +263,7 @@ componentObjects.push(mcqComponent);
         _classes: "",
         _layout: i % 2 === 0 ? "left" : "right",
         title: h1.textContent,
-        displayTitle: !h1.textContent.length > 5 ? "" : h1.textContent,
+        displayTitle: "",
         body: body,
         instruction: "",
         _pageLevelProgress: {
@@ -253,7 +278,7 @@ componentObjects.push(mcqComponent);
       _parentId: blockID,
       _type: "component",
       _component: "graphic",
-      _classes: "",
+      _classes: "mobile-hide",
       _layout: i % 2 === 0 ? "right" : "left",
       title: `graphic for ${blockObj.title}`,
       displayTitle: "",
@@ -271,17 +296,11 @@ componentObjects.push(mcqComponent);
       _isScrollable: false,
       _defaultScrollPercent: 0,
       _pageLevelProgress: {
-        _isEnabled: true,
+        _isEnabled: false,
       },
     };
     graphicObjects.push(graphicComponent);
-}
-
-
-
-
-
-
+  }
 
   // Display JSON output
   document.getElementById("blockObjectsOutput").value = JSON.stringify(
